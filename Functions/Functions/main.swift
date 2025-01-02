c5e14d605f5615fde2e35d631332c010c18ca5d0
erkutahmet <ahmeterkut535@gmail.com>
2025-01-02 23:33:46 +0300

    initial commit

--- Functions/Functions/main.swift
+++ Functions/Functions/main.swift
@@ -82,3 +82,6 @@ print("Summation Result: \(sumResult)")
 f.multiplication(n1: 10, n2: 20)
 f.multiplication(n1: 10.0, n2: 20.2)
 f.multiplication(n1: 10, n2: 20, name: "Erkut")
+
+
+print("Selamualeykü")
